19d0648ba729e86b04bbfb7352cf1df731b016d1
Garvit <agarvit1142000@gmail.com>
2023-06-18 00:33:29 +0530

    added navbar

---
+++ src/layouts/Navbar.jsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+import { Flex, Text, IconButton } from "@chakra-ui/react";
+import { Logo } from "../images/logo";
+import { ChevronLeftIcon } from "@chakra-ui/icons";
+
+export const Navbar = ({ backButton, title, rightElement }) => {
+  return (
+    <Flex
+      as="nav"
+      w={"full"}
+      h={"5rem"}
+      px={"5rem"}
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      bg="white"
+      color="white"
+      borderBottom={"1px solid #EBEBEB"}
+    >
+      <Flex align="center" mr={5}>
+        {backButton && (
+          <Link to="/">
+            <IconButton icon={<ChevronLeftIcon />} variant="ghost" />
+          </Link>
+        )}
+        <Logo />
+        <Text ml={4} fontSize="2xl" fontWeight="bold" color={"#717171"}>
+          {title}
+        </Text>
+      </Flex>
+      <Flex align="center">{rightElement}</Flex>
+    </Flex>
+  );
+};
