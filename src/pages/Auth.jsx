71dfc45b9aa910dbe8ed430a93ca3b8b8635d6eb
Garvit <agarvit1142000@gmail.com>
2023-06-18 00:57:55 +0530

    added auth page

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,7 +1,46 @@
+import { Box, Flex, Text, Button } from "@chakra-ui/react";
+import { Navbar } from "../layouts/Navbar";
 export const Auth = () => {
   return (
     <div>
-      <h1>Auth</h1>
+      <Navbar backButton={false} title="toddle" />
+      <Flex h={"calc(100vh - 5rem)"} justify="center" align="center">
+        <Box
+          w={[300, 400, 500]}
+          border={"1px solid #EBEBEB"}
+          bg="white"
+          rounded="md"
+        >
+          <Box
+            h={64}
+            w={"full"}
+            rounded="md"
+            backgroundImage={
+              "url(https://cloud.toddleapp.com/assets/webapp/login_page/TeacherLeftBanner.svg)"
+            }
+            backgroundSize="cover"
+            backgroundPosition="center"
+          />
+          <Flex
+            direction="column"
+            align="center"
+            justify="center"
+            p={8}
+            bg="white"
+            rounded="md"
+          >
+            <Text fontSize="2xl" fontWeight="bold" color={"#717171"}>
+              Welcome to toddle!
+            </Text>
+            <Text fontSize="md" color={"#717171"} mt={2}>
+              Please sign in to continue
+            </Text>
+            <Button mt={12} w={"full"} rounded="md">
+              Sign in with Google
+            </Button>
+          </Flex>
+        </Box>
+      </Flex>
     </div>
   );
 };
