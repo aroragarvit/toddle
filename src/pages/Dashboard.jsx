e7d9c323e5df0863d0c3cd59f4aaea0c38ca55e5
garvit <agarvit1142000@gmail.com>
2023-06-18 13:36:47 +0530

    Dashboard to get user boards

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,49 @@
 import { AddBoard } from "../components/AddBoard";
 import { Navbar } from "../layouts/Navbar";
+import { useEffect, useState } from "react";
+import { getBoards } from "../functions/getBoards.jsx";
 
 export const Dashboard = () => {
-  return <Navbar title={"toddle"} rightElement={<AddBoard />} />;
+  const [boards, setBoards] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getBoardsData = async () => {
+      const boards = await getBoards();
+      console.log(boards);
+      setBoards(boards);
+      setLoading(false);
+    };
+
+    getBoardsData();
+  }, []);
+
+  return (
+    <div>
+      {" "}
+      <Navbar title={"toddle"} rightElement={<AddBoard />} />
+      {loading ? (
+        <div>loading...</div>
+      ) : (
+        <div>
+          {" "}
+          {boards.map((board) => {
+            return (
+              <div
+                key={board.id}
+                style={{
+                  backgroundColor: board.color,
+                  width: "200px",
+                  height: "200px",
+                  borderRadius: "10px",
+                }}
+              >
+                {board.boardName}
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
 };
