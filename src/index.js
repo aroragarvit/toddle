aa2d6648235305a1b711ea45957b07334d895411
Garvit <agarvit1142000@gmail.com>
2023-06-18 08:52:24 +0530

    extended theme

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react"
 import {Router} from "./Router";
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      100: "#D54D52",
+      900: "#B03E3D",
+    },
+  },
+})
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Router />
     </ChakraProvider>
   </React.StrictMode>
