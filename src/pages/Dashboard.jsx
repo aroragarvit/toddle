2d76ce98e0af71199ebd63853b08fe4a1a664590
Garvit <agarvit1142000@gmail.com>
2023-06-18 08:53:02 +0530

    added Primary button to dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,20 @@
 import { Navbar } from "../layouts/Navbar";
 import { Button } from "@chakra-ui/react";
 import { AddIcon } from "@chakra-ui/icons";
+import { PrimaryButton } from "../commone/PrimaryButton";
 
 export const Dashboard = () => {
   return (
     <div>
       <Navbar
         title={"toddle"}
-        rightElement={<Button leftIcon={<AddIcon />}>Create new Board</Button>}
+        rightElement={
+          <PrimaryButton
+            icon={<AddIcon />}
+            onClick={() => console.log("create new board")}
+            label={"Create new Board"}
+          />
+        }
       />
     </div>
   );
