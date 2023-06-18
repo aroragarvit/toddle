d2084b58422a837c93c851565663d25fb1d5e599
garvit <agarvit1142000@gmail.com>
2023-06-18 15:36:40 +0530

    search For board

--- src/functions/createBoard.jsx
+++ src/functions/createBoard.jsx
@@ -3,11 +3,13 @@ import firebase from "firebase/compat/app";
 
 export const createBoard = async (boardName) => {
   const user = await firebase.auth().currentUser;
+  const searchName = boardName.toLowerCase().replace(/[^a-z0-9]/g, "");
+
   const board = await firestore.collection("boards").add({
     boardName: boardName,
     createdBy: user.uid,
     createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-
+    searchName: searchName,
     posts: [],
   });
   const boardId = board.id;
