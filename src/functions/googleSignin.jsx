83e581f4b796373fd600a367b029b1ea71df4a21
garvit <agarvit1142000@gmail.com>
2023-06-18 11:55:12 +0530

    Improvement in google sign in

--- src/functions/googleSignin.jsx
+++ src/functions/googleSignin.jsx
@@ -1,8 +1,27 @@
 import firebase from "firebase/compat/app";
+import { firestore } from "../config/firebaseConfig";
 export const handleGoogleSignIn = async () => {
   const provider = new firebase.auth.GoogleAuthProvider();
   try {
     await firebase.auth().signInWithPopup(provider);
+    const user = await firestore
+      .collection("users")
+      .doc(firebase.auth().currentUser.uid)
+      .get();
+    if (!user.exists) {
+      await firestore
+        .collection("users")
+        .doc(firebase.auth().currentUser.uid)
+        .set({
+          boardsName: [],
+          boardsId: [],
+          userId: firebase.auth().currentUser.uid,
+          userPosts: [],
+          bookmarks: [],
+          email: firebase.auth().currentUser.email,
+          name: firebase.auth().currentUser.displayName,
+        });
+    }
   } catch (error) {
     console.error(error);
   }
