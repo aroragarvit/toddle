fe03e7cfa13b0ec0fea084a1b77fc252ad964be5
garvit <agarvit1142000@gmail.com>
2023-06-18 16:40:38 +0530

    Adding Id  to board object

--- src/functions/createBoard.jsx
+++ src/functions/createBoard.jsx
@@ -1,18 +1,22 @@
 import { firestore } from "../config/firebaseConfig.jsx";
 import firebase from "firebase/compat/app";
 
-export const createBoard = async (boardName) => {
+export const createBoard = async (boardName, color) => {
   const user = await firebase.auth().currentUser;
   const searchName = boardName.toLowerCase().replace(/[^a-z0-9]/g, "");
 
-  const board = await firestore.collection("boards").add({
+  const boardRef = firestore.collection("boards").doc();
+  const boardId = boardRef.id;
+  await boardRef.set({
+    boardId: "",
+    boardColor: color,
     boardName: boardName,
     createdBy: user.uid,
     createdAt: firebase.firestore.FieldValue.serverTimestamp(),
     searchName: searchName,
     posts: [],
+    boardId: boardId,
   });
-  const boardId = board.id;
 
   const userRef = await firestore.collection("users").doc(user.uid);
 
